185ae874b28e3dd05240a9d38ef877cd2539936f
Kristian Lovey <kristian.lovey@fer.unizg.hr>
2026-04-29 00:12:38 +0200

    update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [390, 640, 828, 1080, 1280, 1440, 1920],
     imageSizes: [48, 96, 128, 256],
+    qualities: [40, 50, 65, 75],
   },
   experimental: {
     optimizePackageImports: ['lucide-react', '@supabase/supabase-js'],
